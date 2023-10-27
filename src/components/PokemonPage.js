1b425f20d5abb72c164929711e117d0e61584a41
Ruth <ruro122020@gmail.com>
2023-10-27 08:33:53 -0400

    fetch pokemons from server

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:3001/pokemon')
+    .then(res => res.json())
+    .then(pokemons => setPokemons(pokemons))
+  }, [])
+  console.log(pokemons)
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
